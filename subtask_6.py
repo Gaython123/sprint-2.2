923a572d1ef564c051029f36df7284b9cc27c5f0
lekorda <lesik2709@gmail.com>
2024-09-19 15:12:28 +0200

    1.st ready

--- subtask_6.py
+++ subtask_6.py
@@ -7,25 +7,27 @@ while True:
         case 1:
             while True:
                 number = int(input("Please enter a number or 'end' to sort the numbers: "))
-                if number == 00:
-                    print(list_of_numbers)
+
+                if number == 12:
+                    if len(list_of_numbers) == 0:
+                        print("The list is empty")
+
+                    else:
+                        print(f"\nUnsorted list is {list_of_numbers}")
+                        for i in range(len(list_of_numbers)):
+                            for j in range(len(list_of_numbers)):
+                                if list_of_numbers[i] > list_of_numbers[j]:
+                                    list_of_numbers[i], list_of_numbers[j] = list_of_numbers[j], list_of_numbers[i]
+                        print(f"Sorted list is {list_of_numbers}\n")
 
                 elif number == int(number):
                     list_of_numbers.append(number)
-                    for i in list_of_numbers:
-                        a = 0
-                        b = user_list
-                        if a > b:
-                            a = b
-                            list_of_numbers[a], list_of_numbers[b] = list_of_numbers[b], list_of_numbers[a]
-
+                    
                 else:
                     print("Invalid input")
 
 
+
         ##case 11:
-
-
-
 
 

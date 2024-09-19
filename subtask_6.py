8a7ec1871027420f5d11cde97f2b9249617f73eb
lekorda <lesik2709@gmail.com>
2024-09-19 12:02:06 +0200

    1st bla

--- subtask_6.py
+++ subtask_6.py
@@ -1,1 +1,26 @@
-print("hello world")
+
+list_of_numbers = []
+while True:
+
+    user_list = int(input("choose option: "))
+    match user_list:
+        case 1:
+            while True:
+                number = input("Please enter numbers you need to sort")
+                list_of_numbers.append(number)
+                for i in list_of_numbers:
+                    a = 0
+                    b = user_list
+                    if a > b:
+                        a = b
+                        list_of_numbers[a], list_of_numbers[b] = list_of_numbers[b], list_of_numbers[a]
+
+                    match number:
+                        case "end":
+                            print(list_of_numbers)
+        ##case 11:
+
+
+
+
+

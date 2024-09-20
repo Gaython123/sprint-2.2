7391cb2dd8984659794680ac07e2022fccacad9e
lekorda <lesik2709@gmail.com>
2024-09-20 10:53:00 +0200

    hhh

--- subtask_6.py
+++ subtask_6.py
@@ -1,12 +1,13 @@
+from utils6 import *
 
-list_of_numbers = []
+#list_of_numbers = []
 while True:
 
     user_list = input("choose option: ")
 
     match user_list:
         case "1":
-
+            list_of_numbers = []
             while True:
                 number = input("Please enter a number or 'end' to sort the numbers: ")
 
@@ -18,24 +19,10 @@ while True:
                         sort_choice = input("Enter '1' or '2' to sort from: \n1. 'min' to 'max' (increasing)\n2. 'max' to 'min' (decreasing)")
                         match sort_choice:
                             case "1":
-                                print(f"\nUnsorted list is {list_of_numbers}")
-                                for i in range(len(list_of_numbers)):
-                                    for j in range(len(list_of_numbers)):
-                                        if i and j >= 0:
-                                            if list_of_numbers[i] < list_of_numbers[j]:
-                                                list_of_numbers[i], list_of_numbers[j] = list_of_numbers[j], list_of_numbers[i]
-                                        else:
-                                            if list_of_numbers[i] > list_of_numbers[j]:
-                                                list_of_numbers[i], list_of_numbers[j] = list_of_numbers[j], list_of_numbers[i]
-                                    print(f"Sorted list is {list_of_numbers}\n")
+                                bubble_sort_increasing(list_of_numbers)
 
                             case "2":
-                                print(f"\nUnsorted list is {list_of_numbers}")
-                                for i in range(len(list_of_numbers)):
-                                    for j in range(len(list_of_numbers)):
-                                        if list_of_numbers[i] > list_of_numbers[j]:
-                                            list_of_numbers[i], list_of_numbers[j] = list_of_numbers[j], list_of_numbers[i]
-                                print(f"Sorted list is {list_of_numbers}\n")
+                                bubble_sort_decreasing(list_of_numbers)
 
                 elif number.isdigit() or number.lstrip("-").isdigit():
                     list_of_numbers.append(number)
--- utils6.py
+++ utils6.py
@@ -1,20 +1,22 @@
 
 
-list_of_numbers = [-4, -9, -3, -199, -200, -400, -23, 4, 7]
+list_of_numbers = [-4, -9, -3, -199, -200, -400, -23, 4, 7, -400, -300, -200, 7, 5, 9, 140, 499]
 def bubble_sort_decreasing(list_of_numbers):
-
+    print(f"\nUnsorted list is {list_of_numbers}")
     for i in range(len(list_of_numbers)):
         for j in range(len(list_of_numbers)):
             if list_of_numbers[i] > list_of_numbers[j]: #decreasing
                 list_of_numbers[i], list_of_numbers[j] = list_of_numbers[j], list_of_numbers[i]
-    print(list_of_numbers)
+    print(f"Sorted list is {list_of_numbers}")
 
 def bubble_sort_increasing(list_of_numbers):
-
+    print(f"\nUnsorted list is {list_of_numbers}")
     for i in range(len(list_of_numbers)):
         for j in range(len(list_of_numbers)):
             if list_of_numbers[i] < list_of_numbers[j]: #decreasing
                 list_of_numbers[i], list_of_numbers[j] = list_of_numbers[j], list_of_numbers[i]
-    print(list_of_numbers)
+    print(f"Sorted list is {list_of_numbers}")
 
-bubble_sort_increasing(list_of_numbers)
+def bubble_function:
+
+

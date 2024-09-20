26e52eff523111955608bd7abd8e2b19c20d0b25
lekorda <lesik2709@gmail.com>
2024-09-20 23:15:52 +0200

    almst

--- subtask_6.py
+++ subtask_6.py
@@ -1,9 +1,10 @@
 from utils6_bubble import *
 from utils6_selection import *
+from utils6_insrtion import *
 
 numbers_bubbles = []
 numbers_selection = []
-numbers = []
+numbers_insertion = []
 while True:
     user_list = input("choose option: ")
 
@@ -15,7 +16,10 @@ while True:
             start_selection(numbers_selection, len(numbers_selection))
 
         case "3":
-            
+            start_bubble(numbers_insertion)
+
+        case "0":
+            break
 
 
 
--- utils6_insrtion.py
+++ utils6_insrtion.py
@@ -15,6 +15,43 @@ def insertion_sort_increasing(array):
 
         # Place key at after the element just smaller than it.
         array[j + 1] = key
-
-insertion_sort_increasing(array)
 print(array)
+
+def insertion_sort_decreasing(array):
+    for step in range(1, len(array)):
+        key = array[step]
+        j = step - 1
+
+        # Compare key with each element on the left of it until an element smaller than it is found
+        # For descending order, change key<array[j] to key>array[j].
+        while j >= 0 and key > array[j]:
+            array[j + 1] = array[j]
+            j = j - 1
+
+        # Place key at after the element just smaller than it.
+        array[j + 1] = key
+print(array)
+
+def start_insertion(array):
+    while True:
+        number = input("Please enter a number or 'end' to sort the numbers: ")
+
+        if number == "end":
+            if len(array) == 0:
+                print("The list is empty")
+
+            else:
+                sort_choice = input(
+                    "Enter '1' or '2' to sort from: \n1. 'min' to 'max' (increasing)\n2. 'max' to 'min' (decreasing)")
+                match sort_choice:
+                    case "1":
+                        insertion_sort_increasing(array)
+
+                    case "2":
+                        insertion_sort_decreasing(array)
+
+        elif number.isdigit() or number.lstrip("-").isdigit():
+            array.append(number)
+
+        else:
+            print("Invalid input")

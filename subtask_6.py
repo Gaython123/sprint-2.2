84eceffdb1d31c72492521d80ae6b097fc0d2b69
lekorda <lesik2709@gmail.com>
2024-09-20 12:36:06 +0200

    opa 1 a 2

--- subtask_6.py
+++ subtask_6.py
@@ -1,11 +1,19 @@
 from utils6_bubble import *
+from utils6_selection import *
 
-list_of_numbers = []
+numbers_bubbles = []
+numbers_selection = []
+numbers = []
 while True:
     user_list = input("choose option: ")
 
     match user_list:
         case "1":
-            start_bubble(list_of_numbers)
+            start_bubble(numbers_bubbles)
 
         case "2":
+            start_selection(numbers_selection, len(numbers_selection))
+
+
+
+

ebf59a3490916e989930b03951dc1bd1e7644b6b
lekorda <lesik2709@gmail.com>
2024-09-22 10:53:52 +0200

    done

--- subtask_7.py
+++ subtask_7.py
@@ -1,6 +1,6 @@
 import random
 from subtask_7_linear import *
-
+from subtask_7_binary import *
 
 while True:
     menu()
@@ -13,12 +13,30 @@ while True:
             print(array)
 
             target = int(input("\nEnter a number to get its index, "
-                               "\nor index '-1' if not in the array"))
+                               "\nor '0' to finish"))
 
             linear_search(array, target)
 
         case "2":
-            
+            array = list(range(1, 10))
+            print(array)
+
+            target = int(input("\nEnter a number to get its index, \nor '0' to finish\n"))
+            binary_search(array, target)
+
+        case "3":
+            array = list(range(1, 10))
+            random.shuffle(array)
+            print(array)
+
+            target = int(input("\nEnter a number to get its index, \nor '0' to finish \n"))
+            binary_search(array, target)
+            print("\nBinary search doesnt work for unsorted arrays\n")
+
+        case "0":
+            break
+
+
 
 
 
--- subtask_7_linear.py
+++ subtask_7_linear.py
@@ -1,15 +1,26 @@
 import random
 
 def menu():
-    print("Enter your choice:\n"
-                    "1. Linear search "
-                    "2. Binary search "
-                    "3. ")
+    """
+
+    :return: all the options available
+    """
+    print("Enter your choice:"
+                    "\n1. Linear search "
+                    "\n2. Binary search with sorted list"
+                    "\n3. Binary search with unsorted list")
 
 
 
 
 def linear_search(array, target):
+    """
+    :param array: - list of numbers where to find the target
+    :param target: - a number to search for
+    :return: index of the searched number, or '-1' if target is not found
+    """
+    print(linear_search.__doc__)
+
     if target in array:
         target_index = array.index(target)
         print(f"\n'{target}'s index is '{target_index}'\n")
